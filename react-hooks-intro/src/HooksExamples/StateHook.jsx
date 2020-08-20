82fe3b3e91e0655ca0bcc52e929a29f485aba81d
continMarcelo <marcelo.contin@bridge.ufsc.br>
2020-08-20 15:58:22 -0300

    Add State Hook

--- react-hooks-intro/src/HooksExamples/StateHook.jsx
+++ react-hooks-intro/src/HooksExamples/StateHook.jsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 
 function Example() {
     
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
+    const [fruit, setFruit] = useState('banana')
+    const [todos, setTodos] = useState([{ text: 'Learn Hooks' }])
+
+    const incrementaCount = () => setCount(count + 1)
     
     return (<div>
         <p>You clicked {count} times!</p>
         <button onClick={
-            () => setCount(count + 1)
+            () => incrementaCount()
         }>Click me!</button>
     </div>)
 }
